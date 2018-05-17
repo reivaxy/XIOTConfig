e3ca87669ecac3d58a98873bbec6598e24bf97bb
Xavier <xavier@c-est-simple.com>
2018-05-17 11:43:43 +0200

    Exposing default SSID for 30s should be enough

--- XIOTConfig.h
+++ XIOTConfig.h
@@ -16,7 +16,7 @@
 // Should be in some common header file...
 #define DEFAULT_APSSID "iotinator" 
 #define DEFAULT_APPWD "iotinator"
-#define DEFAULT_AP_EXPOSITION 60000
+#define DEFAULT_AP_EXPOSITION 30000
 
 #define SSID_MAX_LENGTH 20
 #define PWD_MAX_LENGTH 50
